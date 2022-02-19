4972c2600ec701d752d1d4fbc857b4293bfad2ee
ubuntu20 <tommy20051987@outlook.com>
2022-02-19 21:46:26 +0800

    fix

--- src/boot.asm
+++ src/boot.asm
@@ -14,7 +14,19 @@ mov sp, 0x7c00
 mov ax, 0xb800
 mov ds,ax
 mov byte [0], 'H'
-
+mov byte [2], 'E'
+mov byte [4], 'L'
+mov byte [6], 'L'
+mov byte [8], 'O'
+mov byte [10], ','
+mov byte [12], 'L'
+mov byte [14], 'E'
+mov byte [16], 'W'
+mov byte [18], 'I'
+mov byte [20], 'S'
+mov byte [22], 'S'
+mov byte [24], 'E'
+mov byte [26], 'N'
 jmp $
 
 times 510 - ($ - $$) db 0
